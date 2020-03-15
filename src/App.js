0790e6b9f90e67bd3e8c2ff819c1128b3a5f5e8a
kellynvd <kellynvd@gmail.com>
2020-03-15 10:31:34 -0300

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
+import words from './words'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Developer's Dictionary</h1>
+      <input type='text' placeholder='Search...' />
+      <h2>{words[0].word}</h2>
+      <p>{words[0].definition}</p>
     </div>
   );
 }
